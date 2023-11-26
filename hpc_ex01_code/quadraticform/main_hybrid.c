1b1b115b2c21f2145eb5f3cdccddf3a304a24086
Giorgos Mermigkis <giwrgosmerm@gmail.com>
2023-11-26 15:01:05 +0200

    merm1

--- hpc_ex01_code/quadraticform/main_hybrid.c
+++ hpc_ex01_code/quadraticform/main_hybrid.c
@@ -1,6 +1,8 @@
 // sequential code
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <omp.h>
 
 int main(int argc, char** argv)
 {
@@ -14,26 +16,49 @@ int main(int argc, char** argv)
 	double *v = (double *)malloc(n*sizeof(double));
 	double *w = (double *)malloc(n*sizeof(double));
 
-	/// init A_ij = (i + 2*j) / n^2
-	for (int i=0; i<n; ++i)
-		for (int j=0; j<n; ++j)
-			A[i*n+j] = (i + 2.0*j) / (n*n);
-
-	/// init v_i = 1 + 2 / (i+0.5)
-	for (int i=0; i<n; ++i)
-		v[i] = 1.0 + 2.0 / (i + 0.5);
-
-	/// init w_i = 1 - i / (3.*n)
-	for (int i=0; i<n; ++i)
-		w[i] = 1.0 - i / (3.0*n);
-
-	/// compute
+	double start; 
+	double end; 
 	double result = 0.;
-	for (int i=0; i<n; ++i)
-		for (int j=0; j<n; ++j)
-			result += v[i] * A[i*n + j] * w[j];
+	start = omp_get_wtime(); 
 
-	printf("Result = %lf\n", result);
+	#pragma omp parallel num_threads(3)
+	{
+
+		#pragma omp single
+		{
+			/// init A_ij = (i + 2*j) / n^2
+			for (int i=0; i<n; ++i)
+				for (int j=0; j<n; ++j)
+					A[i*n+j] = (i + 2.0*j) / (n*n);
+		}
+
+		#pragma omp single
+		{
+			/// init v_i = 1 + 2 / (i+0.5)
+			for (int i=0; i<n; ++i)
+				v[i] = 1.0 + 2.0 / (i + 0.5);
+		}
+
+		#pragma omp single
+		{
+			/// init w_i = 1 - i / (3.*n)
+			for (int i=0; i<n; ++i)
+				w[i] = 1.0 - i / (3.0*n);
+		}
+
+		#pragma omp barrier
+		/// compute
+		#pragma omp master
+		{
+			for (int i=0; i<n; ++i)
+				for (int j=0; j<n; ++j)
+					result += v[i] * A[i*n + j] * w[j];
+		}
+	}
+	
+	end = omp_get_wtime();
+
+	printf("Result = %lf\nTime = %lf\n", result, end-start);
 
 	/// free memory
 	free(A);
