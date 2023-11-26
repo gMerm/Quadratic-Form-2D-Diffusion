fcb29d2f200a8634bae7e57e0b5257a4c52ba9a1
CHRYSOVALANTIS MICHAIL LIAKOPOULOS <chrliakopoulos@outlook.com>
2023-11-26 16:09:13 +0200

    using parallel with shared result

--- hpc_ex01_code/quadraticform/main_hybrid.c
+++ hpc_ex01_code/quadraticform/main_hybrid.c
@@ -22,46 +22,53 @@ int main(int argc, char** argv)
 	double result = 0.;
 	start = omp_get_wtime(); 
 
-	#pragma omp parallel num_threads(3)
+	#pragma omp parallel num_threads(8)
 	{
-
-		#pragma omp single
-		{
-			startA=omp_get_wtime();
+		#pragma omp for
+			//startA=omp_get_wtime();
 			/// init A_ij = (i + 2*j) / n^2
 			for (int i=0; i<n; ++i)
 				for (int j=0; j<n; ++j)
-					A[i*n+j] = (i + 2.0*j) / (n*n);
-			
-			endA=omp_get_wtime();
-		}
+					A[i*n+j] = (i + 2.0*j) / (n*n);	
+			//endA=omp_get_wtime();
+	}
 
-		#pragma omp single
+	#pragma omp parallel
+	{
+		#pragma omp single nowait
 		{
 			/// init v_i = 1 + 2 / (i+0.5)
 			for (int i=0; i<n; ++i)
 				v[i] = 1.0 + 2.0 / (i + 0.5);
 		}
 
-		#pragma omp single
+		#pragma omp single nowait
 		{
 			/// init w_i = 1 - i / (3.*n)
 			for (int i=0; i<n; ++i)
 				w[i] = 1.0 - i / (3.0*n);
 		}
 
-		#pragma omp barrier
+		
+	}
 		/// compute
-		#pragma omp master
-		{	
-			startRes=omp_get_wtime();
+		
+		double local_res=0.;
+		startRes=omp_get_wtime();
+		#pragma omp parallel firstprivate(local_res) shared(result)
+		{
+			#pragma omp for schedule(dynamic,8) 
 			for (int i=0; i<n; ++i)
 				for (int j=0; j<n; ++j)
-					result += v[i] * A[i*n + j] * w[j];
+					local_res += v[i] * A[i*n + j] * w[j];
+					
+			#pragma omp critical
+				result = result + local_res;
 
-			endRes=omp_get_wtime();
 		}
-	}
+			
+		endRes=omp_get_wtime();
+	
 
 	end = omp_get_wtime();
 
--- hpc_ex01_code/quadraticform/run.sh
+++ hpc_ex01_code/quadraticform/run.sh
@@ -1,9 +1,9 @@
 make clean
 make
-
+clear
 echo "\nExecuting Serial: \n"
 
 time ./qf_seq
 
 echo "\nExecuting Parallel: \n"
-./main_hybrid
+time ./main_hybrid
